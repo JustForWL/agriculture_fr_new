4e0b925737a26fd0e40773aa7e899c85bfcbc1a4
JustForWL <lvrenbin@163.com>
2014-08-03 00:05:39 +0800

    Revert "agriculture_fr"
    This reverts commit 646bdf685a79e53f9a0b575e19ea252a333dafd9.

--- conn.php
+++
@@ -1,9 +0,0 @@
-<?php 
-$username="root";
-$userpass="root";
-$dbhost="localhost";
-$dbdatabase="wordpress";
-$db_connect = mysql_connect($dbhost,$username,$userpass) or die("Unable to connect to the MySQL!");
-mysql_query("set names 'utf8'");
-mysql_select_db($dbdatabase, $db_connect);
-?>
